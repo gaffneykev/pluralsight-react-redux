cfb0384a8a8bcde14cd0bfabbb2c420aad6cf621
Kevin Gaffney <kevin.gaffney@workday.com>
2018-01-25 11:44:38 +0000

    creating from inputs and a manage course form

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import Courses from './components/course/CoursesPage';
+import ManageCoursePage from "./components/course/ManageCoursePage";
 
 export default (
 
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="courses" component={Courses} />
+    <Route path="course/:id" component={ManageCoursePage} />
     <Route path="about" component={AboutPage} />
   </Route>
 );
